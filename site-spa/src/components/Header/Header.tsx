284b8d7581b1ca2b535bfa155b378d13ff8b63df
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-11-12 20:37:04 -0300

    feat: cria rota Error

--- site-spa/src/components/Header/Header.tsx
+++ site-spa/src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
+import Navbar from "../NavBar/Navbar";
 
 
 export default function Header() {
     return(
-        
+        <nav>
+            <Navbar/>
+        </nav>
     );
 }
---
+++ site-spa/src/routes/Error/index.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+    return(
+        <main>
+            <p>Page not found. Error 404</p>
+        </main>
+    );
+}
