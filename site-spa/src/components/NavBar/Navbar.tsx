b05441c95a4d571241ba6ca123cc6ab1e9b19132
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-11-12 19:57:04 -0300

    feat: adiciona menu hamburguer ao NavBar

--- site-spa/src/components/NavBar/Navbar.tsx
+++ site-spa/src/components/NavBar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { XMarkIcon } from "@heroicons/react/16/solid";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -15,24 +16,49 @@ export default function Navbar() {
         <nav>
             <div>
                 <div>
-                    AptiTrail
+                    <div>
+                        <p>AptiTrail</p>
+                    </div>
+
+                    <div>
+                        {links.map((link) => (
+                            <Link
+                                key={link.to}
+                                to={link.to}
+                            >
+                                {link.label}
+                            </Link>
+                        ))}
+                    </div>
+
+                    <div>
+                        <button
+                            onClick={toggleMenu}
+                            className="text-green-800 focus:outline-none"
+                        >
+                            {isOpen ? (
+                                <XMarkIcon className="h-6 w-6"/>
+                            ) : (
+                                <p className="h-6 w-6">x</p>
+                            )}
+                        </button>
+                    </div>
                 </div>
-                
+            </div>
+            {isOpen && (
                 <div>
                     {links.map((link) => (
                         <Link
                             key={link.to}
                             to={link.to}
+                            className="block text-white hover:text-green-800"
+                            onClick={() => setIsOpen(false)}
                         >
                             {link.label}
                         </Link>
                     ))}
                 </div>
-
-                <div>
-
-                </div>
-            </div>
+            )}
         </nav>
     );
 }
