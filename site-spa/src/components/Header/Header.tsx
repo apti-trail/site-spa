6eb4e0b1020d502c427ae77f59c269ebf47f1bc9
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-11-14 11:21:54 -0300

    feat: adiciona botões cadastro e login no Header

--- site-spa/src/components/Header/Header.tsx
+++ site-spa/src/components/Header/Header.tsx
@@ -4,8 +4,18 @@ import Navbar from "../NavBar/Navbar";
 
 export default function Header() {
     return(
-        <header className="shadow-lg">
-            <Navbar/>
+        <header className="shadow-lg flex items-center px-6">
+            <div className="font-bold text-xl w-1/3">
+                <p>Apti<span className="text-[#2f2c79]">Trail</span></p>
+            </div>
+            <div className="flex-1 flex justify-end md:justify-center">
+                <Navbar/>
+            </div>
+            
+            <div className="hidden md:flex gap-2.5 w-1/3 justify-end">
+                <button className="text-white w-[75px] rounded-[5px] h-[32px]">Login</button>
+                <button className="text-white bg-[#2f2c79] w-[95px] rounded-[5px] h-[32px]">Cadastro</button>
+            </div>
         </header>
     );
 }
