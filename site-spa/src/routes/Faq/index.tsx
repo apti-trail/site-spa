da3c89e327f2c5f5abc50db4f5e2bad0af3d8b1c
JonasSantoos <jscom3121@gmail.com>
2025-11-15 15:48:08 -0300

    feat: cria e estiliza caixas de perguntas

--- site-spa/src/routes/Faq/index.tsx
+++ site-spa/src/routes/Faq/index.tsx
@@ -41,8 +41,34 @@ export default function Faq() {
     };
 
     return(
-        <main>
-            <h1>Perguntas Frequentes</h1>
+        <main className="flex flex-col items-center text-center px-6 py-20">
+            <h1 className="mb-6">Perguntas Frequentes</h1>
+
+            <section className="max-w-[800px] w-full space-y-4"> 
+                {faqs.map((item, index) => (
+                    <div key={index} className="w-full p-6 shadow-md bg-gray-100 rounded-xl transition">
+                        <button onClick={() => toggle(index)} className="w-full flex justify-between items-center text-left bg-transparent">
+                            <h2 className="font-semibold text-[16px] md:text-[18px]">{item.pergunta}</h2>
+
+                            <svg
+                                className={`w-5 h-5 transition-transform duration-300 ${
+                                    openIndex === index ? "rotate-180" : "rotate-0"
+                                }`}
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M19 9l-7 7-7-7"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                ))}
+            </section>
 
         </main>
     );
