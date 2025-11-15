5c5e68b6b7e4d8502ceadabbd2ad755713dba0cf
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-11-15 13:29:33 -0300

    fix: corrige footer para ficar no fim da página

--- site-spa/src/App.tsx
+++ site-spa/src/App.tsx
@@ -5,10 +5,14 @@ import Header from "./components/Header/Header";
 
 export default function App() {
   return(
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header/>
-      <Outlet/>
+
+      <main className="flex-1">
+        <Outlet/>
+      </main>
+      
       <Footer/>
-    </>
+    </div>
   );
 }
