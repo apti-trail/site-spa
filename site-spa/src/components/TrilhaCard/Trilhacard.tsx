71016fb26608de1b18142a4ba3b0558299c4fff1
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-11-22 15:42:43 -0300

    feat: melhora responsividade em telas pequenas

--- site-spa/src/components/TrilhaCard/Trilhacard.tsx
+++ site-spa/src/components/TrilhaCard/Trilhacard.tsx
@@ -3,10 +3,16 @@ import type { TrilhaCardProps } from "../../types/trilhaCardProps"
 
 export default function Trilhacard({ titulo, onClick }: TrilhaCardProps) {
     return(
-        <div className="bg-white shadow-md rounded-xl p-5 w-64 h-48 border border-gray-200 
-                       hover:shadow-lg transition cursor-pointer 
-                       flex flex-col">
-            <h2 className="text-lg font-semibold text-gray-800">{titulo}</h2>
+        <div className="bg-white shadow-md rounded-xl p-4 
+                w-full h-40                      /* mobile */
+                border border-gray-200 
+                hover:shadow-lg transition cursor-pointer
+                flex flex-col
+
+                sm:w-60 sm:h-44                  
+                md:w-64 md:h-48                  
+            ">
+            <h2 className="text-base md:text-lg font-semibold text-gray-800">{titulo}</h2>
 
             <div className="w-full h-px  bg-gray-800 my-3"></div>
 
