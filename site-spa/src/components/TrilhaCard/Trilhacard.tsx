10e794f195da388a512344182087c27da46cbdfe
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-11-23 14:42:39 -0300

    fix: corrige card de trilhas

--- site-spa/src/components/TrilhaCard/Trilhacard.tsx
+++ site-spa/src/components/TrilhaCard/Trilhacard.tsx
@@ -15,10 +15,6 @@ export default function Trilhacard({ titulo, onClick }: TrilhaCardProps) {
             <h2 className="text-base md:text-lg font-semibold text-gray-800">{titulo}</h2>
 
             <div className="w-full h-px  bg-gray-800 my-3"></div>
-
-            <div className="flex flex-1 items-center justify-center">
-                <button onClick={onClick} className="mt-8 bg-[#26245f] text-white h-8 px-6 rounded-lg flex items-center justify-center font-medium hover:opacity-90 transition">Entrar</button>
-            </div>
         </div>
     );
 }
